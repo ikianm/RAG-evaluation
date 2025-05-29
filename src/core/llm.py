e85ba48526cd1a3e98b00d65a3a6123870ad689d
ikianm <ikianm2002@gmail.com>
2025-05-29 08:32:03 +0330

    refactor(llm class): load LLM parameters from .env

--- src/core/llm.py
+++ src/core/llm.py
@@ -1,16 +1,12 @@
 from langchain_openai import ChatOpenAI
-
 import os
 
 class LLM:
     
     def __init__(self, temperature: float):
         self.chat_model = ChatOpenAI(
-            model='llama3.2',
-            api_key='ollama',
-            base_url='http://localhost:11434/v1',
-            # model='google/gemini-2.5-flash-preview-05-20',
-            # api_key=os.getenv('OPENROUTER_API_KEY'),
-            # base_url=os.getenv('OPENROUTER_BASE_URL'),
+            model=os.getenv('LLM_MODEL'),
+            base_url=os.getenv('LLM_BASE_URL'),
+            api_key=os.getenv('LLM_API_KEY'),
             temperature=temperature
         )
